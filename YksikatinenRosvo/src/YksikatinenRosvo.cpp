0478ecdbe725670ccda311b1dcd826719c25bc9c
joukoe <jou@diipadaapa.net>
2013-05-29 13:17:00 +0300

    Muutin ... "Kommenttia"

--- YksikatinenRosvo/src/YksikatinenRosvo.cpp
+++ YksikatinenRosvo/src/YksikatinenRosvo.cpp
@@ -5,6 +5,8 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
+//
+//Joo lisäsin kommenttia
 
 #include <iostream>
 using namespace std;
